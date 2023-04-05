898391d649dce71cd693bdf7341fad828b5a2169
ByronMike <auger.michaell@gmail.com>
2023-04-05 16:31:23 +0000

    commit

--- square.js
+++ square.js
@@ -1,3 +1,4 @@
+// A square of squares
 // You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
 
 // However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
@@ -17,6 +18,15 @@
 // 25  =>  true
 // 26  =>  false
 
-var isSquare = function(n){
-    return false; // fix me
+var isSquare = function (n) {
+  if (Math.sqrt(n) * Math.sqrt(n) === Math.abs(n) && Math.round(Math.sqrt(n)) === Math.sqrt(n)) {
+    return true;
+  } else {
+    return false;
   }
+};
+
+console.log(Math.round(Math.sqrt(53)));
+console.log(Math.sqrt(53));
+console.log(Math.abs(53));
+console.log(isSquare(53));
