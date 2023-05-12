761b0286332af8131c7dce07a3ca32ce74e677c2
ByronMike <auger.michaell@gmail.com>
2023-05-12 09:54:18 +0000

    commit

--- simplePigLatin.js
+++ simplePigLatin.js
@@ -3,21 +3,49 @@
 
 const pigIt = (str) => {
     const splittedStr = str.split(" ");
-    // const arr = [];
-    for (const e of splittedStr) {
-        const word = [];
-        const transformedWord = [];
-        word.push(e);
-        for (let i = 0; i < word.length; i++) {
-            transformedWord.push(word[0]);
-            transformedWord.push("ay");
-            transformedWord.unshift(word);
+    const result = [];
+
+    for (const word of splittedStr) {
+        const firstChar = word[0];
+        const remainingChars = word.slice(1);
+
+        if (/[A-Za-z]/.test(firstChar)) {
+            const modifiedWord = remainingChars + firstChar + "ay";
+            result.push(modifiedWord);
+        } else {
+            result.push(word);
         }
-        debugger
-        console.log(transformedWord);
     }
+
+    return result.join(" ");
 };
 
+// const pigIt = (str) => {
+//     const splittedStr = str.split(" ");
+//     const arr = [];
+//     for (const e of splittedStr) {
+//         const firstCharArr = [];
+//         const remainingArr = [];
+//         for (let i = 0; i < e.length; i++) {
+//             if (i === 0) {
+//                 const mergeChar = [];
+//                 mergeChar.push(e[i]);
+//                 if (/[A-Za-z]/.test(e[i])) {
+//                     mergeChar.push("ay");
+//                 }
+//                 const joinedChar = mergeChar.join("");
+//                 firstCharArr.push(joinedChar);
+//             } else if (i > 0 && i < e.length) {
+//                 remainingArr.push(e[i]);
+//             }
+//         }
+//         const modifiedWord = remainingArr.join("") + firstCharArr.join("");
+//         arr.push(modifiedWord);
+//     }
+//     return arr.join(" ");
+// };
+
 // Examples
-pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
-pigIt('Hello world !');     // elloHay orldway !
+// igPay atinlay siay oolcay    // elloHay orldway !
+
+console.log(pigIt("Pig latin is cool"));
