32b26ff6ff7675911d437a684b9b2257d599afab
ByronMike <auger.michaell@gmail.com>
2023-05-22 16:39:44 +0000

    commit

---
+++ findTheParityOutlier.js
@@ -0,0 +1,31 @@
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+// Only for positive numbers 
+const findOutlier = (integers) => {
+  const sumOfIntegers = integers.reduce(
+    (accumulateur, valeurCourante) => accumulateur + valeurCourante
+  );
+  let outlier;
+  if (integers.length === 1) {
+    outlier = integers[O];
+  }
+  if (integers.length > 1) {
+    // If the sum is odd, this is a list of even number, and the outlier is a odd
+    if (sumOfIntegers % 2 !== 0) {
+      outlier = integers.filter((integer) => integer % 2 !== 0);
+    } else {
+      // If the sum is even, this is a list of odd number, and the outlier is a even
+      outlier = integers.filter((integer) => integer % 2 === 0);
+    }
+  }
+  return outlier.at(0);
+};
+
+console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]));
