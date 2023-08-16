0166d4347f7bfec1b5b938bb2cd43c2912e64646
ByronMike <auger.michaell@gmail.com>
2023-08-16 16:32:00 +0000

    commit

---
+++ consonantValue.ts
@@ -0,0 +1,26 @@
+// Given a lowercase string that has alphabetic characters only and no spaces, return the highest value of consonant substrings. Consonants are any letters of the alphabet except "aeiou".
+
+// We shall assign the following values: a = 1, b = 2, c = 3, .... z = 26.
+
+// For example, for the word "zodiacs", let's cross out the vowels. We get: "z o d ia cs"
+
+// -- The consonant substrings are: "z", "d" and "cs" and the values are z = 26, d = 4 and cs = 3 + 19 = 22. The highest is 26.
+// solve("zodiacs") = 26
+
+// For the word "strength", solve("strength") = 57
+// -- The consonant substrings are: "str" and "ngth" with values "str" = 19 + 20 + 18 = 57 and "ngth" = 14 + 7 + 20 + 8 = 49. The highest is 57.
+// For C: do not mutate input.
+
+// More examples in test cases. Good luck!
+
+export function solve(s: string) {
+  let stringTableCorrespondance = {};
+  // for(let i = 1 ; i < 27 ; i++){
+  //     stringTableCorrespondance[`${}`] =
+  // }
+  console.log(parseInt("a", 16));
+  console.log(String.fromCharCode(49));
+}
+
+const data = "strength";
+console.log(solve(data));
