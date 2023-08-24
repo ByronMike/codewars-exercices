39a4e4447a4aa85497cea67b013dd3a444ed1121
ByronMike <auger.michaell@gmail.com>
2023-08-24 17:18:09 +0000

    commit

--- consonantValue.ts
+++ consonantValue.ts
@@ -35,7 +35,7 @@ export function solve(s: string) {
   for(let i = 0; i < s.length; i++){
     let arr = []
     if(consonant.includes(s[i])){
-      arr.push(s[i])
+      // arr.push(s[i])
       
     }
     console.log(arr)
