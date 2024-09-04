c9e4cd7f16271ac4130be6c3d0f1635f38f6f676
ByronMike <auger.michaell@gmail.com>
2024-09-04 20:53:02 +0200

    commit

---
+++ removeDuplicate.ts
@@ -0,0 +1,24 @@
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Examples:
+
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+const data = [1, 2, 1, 1, 3, 2];
+
+export const distinct = (a: number[]): number[] => {
+  let seen: Set<number> = new Set();
+  return a.filter((e) => {
+    if (!seen.has(e)) {
+      seen.add(e);
+      return true;
+    }
+    return false;
+  });
+};
+
+console.log(distinct(data));
